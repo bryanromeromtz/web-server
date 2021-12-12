b8e72f3f5491872e4be225feec3b9ef8ae8f0885
bryanromeromtz <cryptomtz@hotmail.com>
2021-12-12 01:13:23 -0600

    serve static content

--- app.js
+++ app.js
@@ -2,16 +2,18 @@ const express = require('express');
 const app = express();
 const port = 8080
 
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
   res.send('Home Page');
 });
 
-app.get('/path', (req, res) => {
+app.get('hola-mundo', (req, res) => {
   res.send('Route');
 });
 
 app.get('*', (req, res) => {
-  res.send('404 | Page not found');
+  res.sendFile(__dirname + '/public/hola-mundo/404.html');
 })
 
 app.listen(port, (req, res) => {
