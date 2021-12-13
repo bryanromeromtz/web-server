0fe93239fe487d15720f4411896f2b574aa78572
bryanromeromtz <cryptomtz@hotmail.com>
2021-12-12 19:52:40 -0600

    settled mustach by handlebars

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const express = require('express');
 const app = express();
 const port = 8080
 
-app.use(express.static('public'))
+app.set('view engine', 'hbs');
 
+app.use(express.static('public'));
 
+app.get('/', (req, res) => {
+  res.render('home');
+});
 
 app.get('/generic', (req, res) => {
   res.sendFile(__dirname + '/public/generic.html')
